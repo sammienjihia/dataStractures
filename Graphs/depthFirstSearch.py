5370fa6288c836bc0e6cd5f24fc2664f20307e42
sammy njihia <sammy.njihia@jumo.world>
2020-07-15 12:00:09 +0300

    Graph theory

---
+++ Graphs/depthFirstSearch.py
@@ -0,0 +1,71 @@
+"""
+Depth first search/ Explorative graph search utilises a stack, can be a stack that we create or
+a call stack when implemented using recursion.
+Explore all neighbours to a node
+Depth First Search (DFS) uses the concept of backtracking at its very core.
+"""
+
+# Using a call stack/ recursion
+graph = {
+    "A": ['B','C'],
+    "B": ['D', 'E'],
+    "C": ['F'],
+    "D": [],
+    "E": ['F'],
+    "F": []
+}
+
+visited = set()
+
+def dfs(visited, graph, node): # utilises recursion, hence the call stack
+    if node not in visited: # if we haven't visited the node, then explore it
+        print(node)
+        visited.add(node)
+        for neighbour in graph[node]: # with recursion the loop becomes nested
+            dfs(visited, graph, neighbour)
+
+
+# using a fifo queue/ 
+graph2 = {
+    "A": ['B','C'],
+    "B": ['D', 'E'],
+    "C": ['F'],
+    "D": [],
+    "E": ['F'],
+    "F": []
+}
+
+isVisited = set()
+nodeStack = []
+
+def dfsStack(node):
+    # push node to stack
+    nodeStack.append(node)
+
+    while len(nodeStack): # This condition checks 
+        node = nodeStack.pop()
+
+        # check if node has already been visited
+        if node not in isVisited:
+            print(node)
+            # mark the node as visited 
+            isVisited.add(node)
+
+            # explore it's neighbours
+            for neighbour in graph2[node]:
+                # add neigbour to the stack
+                nodeStack.append(neighbour)
+
+
+
+if __name__ == "__main__":
+    dfs(visited, graph, "A")
+    dfsStack('A')
+"""
+
+A, B, D, E, F, C
+
+Resources used: https://www.freecodecamp.org/news/lets-backtrack-and-save-some-queens-1f9ef6af5415/#:~:text=Depth%20First%20Search%20(DFS)%20uses,up%20in%20two%20possible%20states.
+
+"""
+
---
+++ Graphs/graphs.md
@@ -0,0 +1,21 @@
+# Graph
+A graph is a data stracture that consists of a set of vertices and edges-represented as 
+1. ordered pair of vertices- Directed graph
+2. Unordered pair of vertices- Undirected graph.
+
+## Graph terminology
+1. **Adjacency** : A vertex is said to be adjacent to another vertex if there exists an edge connecting them
+2. **Path** : A sequence of edges that allows you to go from a start vertex to a target vertex
+
+## Graph representation
+1. **Adjacency list** : An array of linked lists
+2. **Adjacency matrix**
+
+## Time complexity
+The time complexity of DFS if the entire tree is traversed is O(V)O(V) where V is the number of nodes. In the case of a graph, the time complexity is O(V + E)O(V+E) where V is the number of vertexes and E is the number of edges.
+
+Resources used 
+* [Programiz](https://www.programiz.com/dsa/graph)
+* [MIT lecture notes: Unit 5: Graphs](https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-notes/)
+* [Educative: Graph data stracture](https://www.educative.io/edpresso/what-is-a-graph-data-structure)
+* []()
