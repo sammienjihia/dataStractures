51c9fac5112ff0f703b807cb28dff1c220d95d5a
sammy njihia <sammy.njihia@jumo.world>
2020-09-08 15:23:59 +0300

    an iterative approach to pre-order traversal

--- Trees/preOrderTraversal.py
+++ Trees/preOrderTraversal.py
@@ -55,6 +55,33 @@ def preOrderTraversal(root_node):
 
 print(preOrderTraversal(bst.root))
 
+"""
+NB: Before adding a node to a stack, print the node.
+NB after poping stack, get the right child of the poped node. This is because if a node has been inserted to
+the stack, then it means we've already seen it's leftchild
 
-# def preOrderTraversalIterative(root_node):
-#     f
+"""
+def preOrderTraversalIterative(root_node):
+    my_stack = []
+    my_stack.append(root_node)
+    print(root_node.val)
+
+    while root_node or len(my_stack):
+        root_node = root_node.leftChild if root_node != None else root_node
+        if root_node == None and len(my_stack):
+            # if root is none, 
+            # 1. then pop stack, 
+            # 2. get the right child of the node and 
+            # 3. make it the new root
+            # 4. then add it to stack
+            # 5. Print the root 
+            node = my_stack.pop()
+            root_node = node.rightChild
+            if root_node:
+                my_stack.append(root_node)
+                print(root_node.val)
+        else:
+            my_stack.append(root_node)
+            print(root_node.val)
+
+preOrderTraversalIterative(bst.root)
